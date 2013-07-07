0be78ca1dc9f8b4ecbc311707c5c7142183c9536
Helge Fredheim <helge.fredheim@bekk.no>
2013-07-07 19:25:16 +0200

    Div. små endringer

--- 05-javascript/del1/løsningsforslag/løsningsforslag-bildegalleri.js
+++ 05-javascript/del1/løsningsforslag/løsningsforslag-bildegalleri.js
@@ -2,9 +2,14 @@ function slideShow() {
   var current = $('.imageGallery .show');
   var next = current.next().is('img') ? current.next() : current.parent().find('img:first');
   current.hide().removeClass('show');
-  next.fadeIn('slow').addClass('show');
+  current.fadeOut('slow', function() {
+  	next.fadeIn('slow').addClass('show');	
+  })
+  
 
-  setTimeout(slideShow, 5000);
+  //setTimeout(slideShow, 5000);
 }
+
+setInterval(slideShow, 5000);
 
 slideShow();
